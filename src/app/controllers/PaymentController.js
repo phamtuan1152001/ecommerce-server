fec9bbe2e46712f3abc85134c9dcae46c688a3b3
phamtuan11520011 <phamtuan1152001@gmail.com>
2024-03-17 15:44:25 +0700

    REFACTOR CODE OF PAYMENT MOMO

---
+++ src/app/controllers/PaymentController.js
@@ -0,0 +1,91 @@
+class PaymentController {
+  createPaymentMomo(request, response, next) {
+    //parameters
+    var partnerCode = "MOMO";
+    var accessKey = "F8BBA842ECF85";
+    var secretkey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+    /*  */
+    var orderInfo = "pay with MoMo";
+    var amount = "5000000";
+    var orderId = /* requestId */Math.floor(Math.random() * 90000) + 10000;
+    /*  */
+    var requestId = partnerCode + new Date().getTime();
+    var requestType = "payWithATM"
+    var redirectUrl = "http://localhost:3002/";
+    var ipnUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
+    // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
+    var extraData = ""; //pass empty value if your merchant does not have stores
+
+    //before sign HMAC SHA256 with format
+    //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+    var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType
+    //puts raw signature
+    // console.log("--------------------RAW SIGNATURE----------------")
+    // console.log(rawSignature)
+    //signature
+    const crypto = require('crypto');
+    var signature = crypto.createHmac('sha256', secretkey)
+      .update(rawSignature)
+      .digest('hex');
+    // console.log("--------------------SIGNATURE----------------")
+    // console.log(signature)
+
+    //json object send to MoMo endpoint
+    const requestBody = JSON.stringify({
+      partnerCode: partnerCode,
+      accessKey: accessKey,
+      requestId: requestId,
+      amount: amount,
+      orderId: orderId,
+      orderInfo: orderInfo,
+      redirectUrl: redirectUrl,
+      ipnUrl: ipnUrl,
+      extraData: extraData,
+      requestType: requestType,
+      signature: signature,
+      lang: 'en'
+    });
+    //Create the HTTPS objects
+    const https = require('https');
+    const options = {
+      hostname: 'test-payment.momo.vn',
+      port: 443,
+      path: '/v2/gateway/api/create',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(requestBody)
+      }
+    }
+    //Send the request and get the response
+    const req = https.request(options, res => {
+      console.log(`Status: ${res.statusCode}`);
+      console.log(`Headers: ${JSON.stringify(res.headers)}`);
+      res.setEncoding('utf8');
+      res.on('data', (body) => {
+        // console.log('Body: ');
+        // console.log(body);
+        // console.log('payUrl: ');
+        // console.log(JSON.parse(body).payUrl);
+        response.json({
+          retCode: 0,
+          retText: "Starting payment with MOMO",
+          retData: JSON.parse(body).payUrl,
+        });
+      });
+      res.on('end', () => {
+        console.log('No more data in response.');
+      });
+    })
+
+    req.on('error', (e) => {
+      console.log(`problem with request: ${e.message}`);
+    });
+    // write data to request body
+    console.log("Sending....")
+    req.write(requestBody);
+    req.end();
+  }
+}
+
+module.exports = new PaymentController();
